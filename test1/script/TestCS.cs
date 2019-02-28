381b7402cb0eb025fe1098ba080f883351fa8d1c
朱世伟 <zgamecn@gmail.com>
2019-02-28 15:25:22 +0800

    add
    add

---
+++ test1/script/TestCS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class TestCS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("test cs");
+        GameObject obj = GameObject.Find("CM vcam1");
+        CinemachineVirtualCamera vir = obj.GetComponent<CinemachineVirtualCamera>();
+        Debug.Log(""+ vir.Priority);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ test1/script/VirtualCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class VirtualCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetVirtualCamera()
+    {
+        Debug.Log("test cs");
+        GameObject obj = GameObject.Find("CM vcam1");
+        CinemachineVirtualCamera vir = obj.GetComponent<CinemachineVirtualCamera>();
+        Debug.Log(""+ vir.Priority);
+    }
+}
