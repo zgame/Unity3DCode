778a17ea7c27d8b702cf6734d6a4194cfeda148a
朱世伟 <zgamecn@gmail.com>
2019-04-03 17:19:53 +0800

    add
    add

---
+++ test1/script/Support/CallTriggerEnter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallTriggerEnter : MonoBehaviour
+{
+    public Action<Collider> handler;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (handler != null)
+            handler(other);
+    }
+}
+
+// lua 调用范例:
+// gameObject:AddComponent(typeof(CS.CallTriggerEnter)).handler = function(other) end
